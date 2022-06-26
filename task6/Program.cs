53bd659f247c17f6b85bd710d96a7c42beb9ee5a
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-06-26 18:53:39 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+double firstNumber = -3;
+firstNumber = firstNumber%2;
+if (firstNumber==0) Console.WriteLine("да");
+else Console.WriteLine("нет");
