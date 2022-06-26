01bdc94de5dc7586f5895a3e0a72bae0e814bf2a
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-06-26 19:36:59 +0300

    task8

--- task8/Program.cs
+++ task8/Program.cs
@@ -3,3 +3,11 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8*/
 
+Console.Write("Ведите число ");
+double number=Convert.ToDouble(Console.ReadLine());
+int wholeNumber=(int)number;
+for(int outputNumber=1;outputNumber<=wholeNumber;outputNumber++) {
+int remains=outputNumber%2;
+if (remains==0) Console.Write($"{outputNumber}, ");
+}
+
