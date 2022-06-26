6ec69745829c0026c03ddf3ee9cb7bd6ebff5d14
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-06-26 18:41:13 +0300

    task4 and correction task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,9 +2,18 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
-Console.WriteLine("Ведите первое число");
-double firstNumber=Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Ведите второе число");
-double secondNumber=Convert.ToDouble(Console.ReadLine());
-if (firstNumber>secondNumber) Console.WriteLine($"{firstNumber}, больше чем {secondNumber}");
+
+//в задаче вывод должен показать большее и меньшее число, в примере показывает только большее, условия были непонятны, сделал по задаче
+
+double firstNumber = 5;
+double secondNumber = 7;
+if (firstNumber > secondNumber) Console.WriteLine($"{firstNumber}, больше чем {secondNumber}");
 else Console.WriteLine($"{firstNumber}, меньше чем {secondNumber}");
+
+//если программа берет числа из консоли
+/*Console.WriteLine("Ведите первое число");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+if (firstNumber > secondNumber) Console.WriteLine($"{firstNumber}, больше чем {secondNumber}");
+else Console.WriteLine($"{firstNumber}, меньше чем {secondNumber}");*/
---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+
+double firstNumber = 2;
+double secondNumber = 3;
+double sthirdNumber = 7;
+double max = secondNumber;
+if (firstNumber > secondNumber) max = firstNumber;
+if (max < sthirdNumber) max = sthirdNumber;
+Console.WriteLine(max);
+
+//если программа берет числа из консоли
+/*Console.Write("Ведите первое число ");
+double firstNumber=Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите второе число ");
+double secondNumber=Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите третье число ");
+double sthirdNumber=Convert.ToDouble(Console.ReadLine());
+double max=secondNumber;
+if (firstNumber>secondNumber) max=firstNumber;
+if (max<sthirdNumber) max=sthirdNumber;
+Console.WriteLine($"Максимальное число {max}");*/
