e63aa80a7b93eb1dc76d997805dbdaf435548f7b
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-06-26 19:39:51 +0300

    task8 corection

--- task8/Program.cs
+++ task8/Program.cs
@@ -4,10 +4,11 @@
 8 -> 2, 4, 6, 8*/
 
 Console.Write("Ведите число ");
-double number=Convert.ToDouble(Console.ReadLine());
-int wholeNumber=(int)number;
-for(int outputNumber=1;outputNumber<=wholeNumber;outputNumber++) {
-int remains=outputNumber%2;
-if (remains==0) Console.Write($"{outputNumber}, ");
+double number = Convert.ToDouble(Console.ReadLine());
+int wholeNumber = (int)number;
+for (int outputNumber = 1; outputNumber <= wholeNumber; outputNumber++)
+{
+    int remains = outputNumber % 2;
+    if (remains == 0) Console.Write($"{outputNumber}, ");
 }
 
