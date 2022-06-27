eedba04df0c28028c73f5d7ab768640d46af56cd
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-06-27 14:45:51 +0300

    task7

--- task7/Program.cs
+++ task7/Program.cs
@@ -4,3 +4,17 @@
 456 -> 6
 782 -> 2
 918 -> 8*/
+
+Console.Write("Введите трехзначное число ");
+string number = Console.ReadLine();
+int numberOfCharacters = number.Length;
+
+while (numberOfCharacters != 3)
+{
+    Console.Write("Вы ввели не трехзначное число, попоробуйте еще раз: ");
+    number = Console.ReadLine();
+    numberOfCharacters = number.Length;
+}
+int outputNumber = Convert.ToInt32(number.Remove(0, 2));
+Console.WriteLine(outputNumber);
+
